c8c0d46848ac6ce8f5f90802967dc138043e484d
blackneb <antenehcs@gmail.com>
2023-05-10 09:27:12 +0300

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import NavBar from './widgets/layouts/NavBar'
 import DashboardMain from './layouts/DashboardMain'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
+  const [login, setlogin] = useState(false);
+  const [createAccount, setCreateAccount] = useState(false);
   return (
     <div className="App">
-      <DashboardMain/>
+      {(() => {
+          if(login === false){
+            return(
+              <div>
+                {createAccount? <Signup setCreateAccount={ setCreateAccount }/> : <Login setlog={setlogin} setCreateAccount={setCreateAccount} /> }
+              </div>
+            )
+          }
+          else if(login === true){
+            return(
+              <div>
+                <DashboardMain/>
+              </div>
+            )
+          }
+        })()}
     </div>
   )
 }
