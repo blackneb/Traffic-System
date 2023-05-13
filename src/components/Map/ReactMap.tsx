5275fcc0b33e5c7f93abe6ab622332e76112b1c0
blackneb <antenehcs@gmail.com>
2023-05-13 13:49:21 +0300

    map trial

---
+++ src/components/Map/ReactMap.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Map } from 'react-map-gl'
+
+const ReactMap = () => {
+  return (
+    <div>
+      <Map
+        initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 14
+        }}
+        style={{width: 600, height: 400}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+    </div>
+  )
+}
+
+export default ReactMap
