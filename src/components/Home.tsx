958629ea109612541318de7599b942a4263e3987
blackneb <antenehcs@gmail.com>
2023-05-14 15:15:39 +0300

    Home page design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactMap from './Map/ReactMap'
+import { Card } from 'antd'
 
 const Home = () => {
   return (
     <div>
-      <ReactMap/>
+      <div className='flex flex-row justify-evenly'>
+          <Card className='mx-4 my-4' size="small" title="To Day" style={{ width: 300 }}>
+            <p>Number</p>
+          </Card>
+          <Card className='mx-4 my-4' size="small" title="This Week" style={{ width: 300 }}>
+            <p>Number</p>
+          </Card>
+          <Card className='mx-4 my-4' size="small" title="This Month" style={{ width: 300 }}>
+            <p>Number</p>
+          </Card>
+        </div>
     </div>
   )
 }
